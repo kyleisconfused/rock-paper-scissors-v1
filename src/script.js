53b1bf32052d38f06c61a8711a41bc0b2bab2ea1
k l <103784140+kyleisconfused@users.noreply.github.com>
2022-07-27 18:39:10 -0230

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,93 @@
+let computerScore = 0;
+let userScore = 0;
+let computerSelection;
+let userSelection;
+
+let btns = document.querySelectorAll(".btn");
+const result = document.querySelector(".results-container");
+const resetBtn = document.querySelector(".reset-btn");
+
+btns.forEach((button) => {
+  button.addEventListener("click", () => {
+    const img = button.querySelector("img");
+    userSelection = img.alt.toLowerCase();
+
+    playGame(computerSelection, userSelection);
+
+    if (userScore === 5 || computerScore === 5) {
+      declareWinner();
+    }
+  });
+});
+
+const selectionArray = ["rock", "paper", "scissors"];
+
+const getComputerSelection = function () {
+  return selectionArray[~~(Math.random() * selectionArray.length)];
+};
+
+const playGame = function (computerSelection, userSelection) {
+  computerSelection = getComputerSelection();
+  userSelection = userSelection.toLowerCase();
+  if (computerSelection == userSelection) {
+    displayResult("It's a draw!");
+  } else if (
+    (computerSelection == "rock" && userSelection == "scissors") ||
+    (computerSelection == "paper" && userSelection == "rock") ||
+    (computerSelection == "scissors" && userSelection == "paper")
+  ) {
+    computerScore = ++computerScore;
+    keepComputerScore();
+    displayResult(
+      `Computer wins the round! ${capitalizeSelection(
+        computerSelection
+      )} beats ${userSelection}`
+    );
+  } else {
+    userScore = ++userScore;
+    keepUserScore();
+    displayResult(
+      `Player wins the round! ${capitalizeSelection(
+        userSelection
+      )} beats ${computerSelection}`
+    );
+  }
+};
+
+const capitalizeSelection = function (str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+const declareWinner = function () {
+  if (computerScore > userScore) {
+    result.textContent = `Damn, you lost. Sucks to suck.`;
+  } else {
+    result.textContent = `You won! You truly beat the shit out of that computer.`;
+  }
+
+  resetBtn.textContent = "Play again?";
+};
+
+const displayResult = function (str) {
+  result.textContent = str;
+};
+
+const resetGame = function () {
+  computerScore = 0;
+  userScore = 0;
+  keepComputerScore();
+  keepUserScore();
+  result.textContent = "";
+};
+
+resetBtn.addEventListener("click", resetGame);
+
+const keepComputerScore = function () {
+  let computerScoreDisplay = document.querySelector(".computer-score");
+  computerScoreDisplay.textContent = computerScore;
+};
+
+const keepUserScore = function () {
+  let userScoreDisplay = document.querySelector(".user-score");
+  userScoreDisplay.textContent = userScore;
+};
